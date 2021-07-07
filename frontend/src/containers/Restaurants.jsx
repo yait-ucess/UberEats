877e4c1dc1d2202a44310352638c6d92147f13d7
yait-ucess <bandai28@ezweb.ne.jp>
2021-07-07 14:25:15 +0900

    CORSを使ってフロントとサーバーサイドの通信を可能にした。
    CORSとは異なるURL(オリジン)間での通信を許容する設定のこと。
    一般的にクロスオリジン間でのリソースの共有はセキュリティの観点から禁止されており、これを意図的に許容させるのがCORS。
    
    ・SPAはフロントとサーバーサイドを分離して、CORSを使ってデータを共有させることで実現していると理解した。

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,11 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+
+import { fetchRestaurants } from "../apis/restaurants";
 
 const Restaurants = () => {
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+
   return (
     <Fragment>
       レストラン一覧
     </Fragment>
   )
-}
+};
 
-export default Restaurants
+export default Restaurants;
